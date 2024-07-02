95631d772de4438f383444691a3e30a43d9a2caf
{doshishaus} <cgeh0721@mail3.doshisha.ac.jp>
2024-07-03 00:24:14 +0900

    フェードインの処理、画面何のスクロール、画面のブレ軽減

--- js/fadein.js
+++ js/fadein.js
@@ -1,4 +1,8 @@
-ScrollReveal().reveal('.fade',{
-    duration:1500,
-    delay:200
+ScrollReveal().reveal('.fade', {
+    duration: 1500,
+    delay: 200
+});
+window.addEventListener("beforeunload", function (event) {
+    var body = document.querySelector('body');
+    body.classList.add('fade-out');
 });
